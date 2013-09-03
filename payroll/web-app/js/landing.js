5c135c4a2f71c8a9550da13aef6a144d7b2571a3
brettg381 <brett_groshong@intuit.com>
2013-09-03 00:45:04 -0700

    Added logic to transfer landing form info to paycheck calculator form.

---
+++ payroll/web-app/js/landing.js
@@ -0,0 +1,13 @@
+var transferLandingData = function() {
+    $('#hoursWorked').val($('#landingHoursWorked').val());
+    $('#payRate').val($('#landingPayRate').val());
+    $('#payDate').val($('#landingPayDate').val());
+    $('#payPeriod').val($('#landingPayPeriod').val());
+    $('#payType').val($('#landingPayType').val());
+    $('#fedFilingStatus').val($('#landingFedFilingStatus').val());
+    $('#fedAllowances').val($('#landingFedAllowances').val());
+    $('#fedAdditionalWithheld').val($('#landingFedAdditionalWithheld').val());
+    $('#stateFilingStatus').val($('#landingStateFilingStatus').val());
+    $('#stateAllowances').val($('#landingStateAllowances').val());
+    $('#stateAdditionalWithheld').val($('#landingStateAdditionalWithheld').val());
+}
