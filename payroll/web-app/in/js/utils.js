5c20e15f6b0c721e938871879761e9a69b6b7e8c
brettg381 <brett_groshong@intuit.com>
2013-10-10 18:54:23 -0700

    Added a utility function for setting cookies.

--- payroll/web-app/in/js/utils.js
+++ payroll/web-app/in/js/utils.js
@@ -17,3 +17,9 @@ function getCookie(c_name) {
     }
     return c_value;
 }
+function setCookie(c_name, value, exdays) {
+    var exdate=new Date();
+    exdate.setDate(exdate.getDate() + exdays);
+    var c_value=escape(value) + ((exdays==null) ? "" : "; expires="+exdate.toUTCString());
+    document.cookie=c_name + "=" + c_value;
+}
